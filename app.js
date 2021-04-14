880d4de8c8a36d08c5d12d7bc04f59258f939c39
IuryB <iurybubnov16@gmail.com>
2021-04-13 17:51:56 -0700

    homepage.ejs added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const userRoutes = require("./routes/user");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
